cbf473b51d7e534459efd6954cdd8a3e4a218972
Trevor Strieber <trevor@strieber.org>
2015-04-01 14:05:01 -0700

    Adding whitelist tests.
    * Testing to make sure that attributes added using the whitelist method
      show up in the results.
    * Renaming ModelHelper.get_response to ModelHelper.response.
    * Adding ModelHelper.whitelisted_response.

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,7 +3,7 @@ describe Person do
 
   describe 'when locating a specific person' do
     let!(:person)  { create(:example_person) }
-    let(:response) { ModelHelper.get_response(subject.class, name: 'Example Person') }
+    let(:response) { ModelHelper.response(Person, name: 'Example Person') }
 
     it_behaves_like 'fastapi_meta' do
       let(:expected) { { total: 1, count: 1, offset: 0, error: false } }
@@ -16,7 +16,7 @@ describe Person do
 
   describe 'when filtering through many people' do
     let!(:people)  { create_list(:person, 15) }
-    let(:response) { ModelHelper.get_response(subject.class, age__gte: 0) }
+    let(:response) { ModelHelper.response(Person, age__gte: 0) }
 
     it_behaves_like 'fastapi_meta' do
       let(:expected) { { total: 15, count: 15, offset: 0, error: false } }
@@ -27,9 +27,22 @@ describe Person do
     end
   end
 
+  describe 'when whitelisting person attributes' do
+    let!(:person) { create(:person) }
+    let(:response) { ModelHelper.whitelisted_response(Person, 'created_at') }
+
+    it_behaves_like 'fastapi_meta' do
+      let(:expected) { { total: 1, count: 1, offset: 0, error: false } }
+    end
+
+    it_behaves_like 'fastapi_data' do
+      let(:expected) { { attributes: %w(id name gender age buckets created_at) } }
+    end
+  end
+
   describe 'when locating a person associated with a bucket' do
     let!(:person)            { create(:person_with_buckets) }
-    let(:response)           { ModelHelper.get_response(Bucket) }
+    let(:response)           { ModelHelper.response(Bucket) }
     let(:person_from_bucket) { response['data'].first['person'] }
 
     it_behaves_like 'fastapi_meta' do
