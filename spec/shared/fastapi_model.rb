45b1a5af990bf954e4caa9cc6130145d214afc7c
Trevor Strieber <trevor@strieber.org>
2015-04-01 17:44:00 -0700

    Testing for the existence of FastAPI methods.

--- spec/shared/fastapi_model.rb
+++ spec/shared/fastapi_model.rb
@@ -2,4 +2,24 @@ shared_examples 'fastapi_model' do
   it 'responds to fastapi' do
     expect(subject.class).to respond_to(:fastapi)
   end
+
+  it 'responds to fastapi.data' do
+    expect(subject.class.fastapi).to respond_to(:data)
+  end
+
+  it 'responds to fastapi.data_json' do
+    expect(subject.class.fastapi).to respond_to(:data_json)
+  end
+
+  it 'responds to fastapi.meta' do
+    expect(subject.class.fastapi).to respond_to(:meta)
+  end
+
+  it 'responds to fastapi.meta_json' do
+    expect(subject.class.fastapi).to respond_to(:meta_json)
+  end
+
+  it 'responds to fastapi.to_hash' do
+    expect(subject.class.fastapi).to respond_to(:to_hash)
+  end
 end
