5eb0a1a33a2bbacf9d4b4c35723a83aca6e7de0e
Trevor Strieber <trevor@strieber.org>
2015-04-02 11:59:13 -0700

    Test for non-nested default filter.
    * Noticed that the default filter is only broken when its a nested
      default filter.
    * Added test for regular default filter.
      * Passes.

--- spec/models/marble.rb
+++ spec/models/marble.rb
@@ -3,4 +3,6 @@ class Marble < ActiveRecord::Base
 
   fastapi_standard_interface [:id, :color, :radius, :bucket]
   fastapi_standard_interface_nested [:id, :color, :radius]
+
+  fastapi_default_filters(color__not: 'clear')
 end
