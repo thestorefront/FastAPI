8e697643c71722bf11b293e20f38c22e1e99a398
Trevor Strieber <trevor@strieber.org>
2015-08-23 19:11:06 -0700

    Adding advanced spoofing.
    Spoof vs Spoof!
    ===============
    
    `spoof`'s functionality remains the same, but `spoof!` performs FastAPI
    filtering, whitelisting, and handles preloaded associations.
    
    * Created a new class to handle spoofing.
      * Accepts ActiveRecord data and overriding meta data.
    * `spoof!` includes related data if it is in the
      allowed fields or whitelist, and has been preloaded.

---
+++ lib/fastapi/spoof.rb
@@ -0,0 +1,54 @@
+module FastAPI
+  class Spoof
+
+    def initialize(data, meta = {}, whitelist = nil)
+      @data      = data
+      @meta      = default_meta.merge(meta)
+      @whitelist = whitelist
+    end
+
+    def spoof(data = nil)
+      Oj.dump(meta: @meta, data: data || @data)
+    end
+
+    def spoof!
+      spoof(prepared_data)
+    end
+
+    private
+    def default_meta
+      { total: @data.size, count: @data.size, offset: 0 }
+    end
+
+    def prepared_data
+      @data.map do |row|
+        allowed_fields = allowed_fields(row.class)
+        clean_data(attributes_and_associations(row), allowed_fields)
+      end
+    end
+
+    def allowed_fields(klazz, nested = false)
+      nested ? klazz.fastapi_fields_sub : klazz.fastapi_fields + @whitelist
+    end
+
+    def clean_data(entity, allowed_fields)
+      entity.symbolize_keys.slice(*allowed_fields)
+    end
+
+    def attributes_and_associations(row)
+      row.attributes.merge(loaded_associations(row))
+    end
+
+    def loaded_associations(row)
+      row.association_cache.each_with_object({}) do |(name, association), results|
+
+        allowed_fields = allowed_fields(association.klass, true)
+        target         = association.target
+        raw_data       = [*target].map { |element| element.attributes }
+        cleaned_data   = raw_data.map { |data| clean_data(data, allowed_fields) }
+
+        results[name] = cleaned_data
+      end
+    end
+  end
+end
