1a1be56fd89f007772d66877a478928549590a7f
Trevor Strieber <trevor@strieber.org>
2015-08-23 19:17:10 -0700

    Created advanced spoofing tests.
    * Added a spoof! method to ModelHelper.

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -133,4 +133,119 @@ describe Person do
       let(:expected) { { attributes: %w(id name gender age buckets dishes pets) } }
     end
   end
+
+  describe 'when spoofing! without preload' do
+    let!(:person)          { create(:person_with_buckets) }
+    let(:people)           { Person.all }
+    let(:response)         { ModelHelper.spoof!(Person, people) }
+    let(:retrieved_person) { response["data"].first }
+
+    it_behaves_like 'fastapi_meta' do
+      let(:expected) { { total: 1, count: 1, offset: 0, error: false } }
+    end
+
+    it_behaves_like 'fastapi_data' do
+      let(:expected) { { attributes: %w(id name gender age) } }
+    end
+
+    it 'has the correct count' do
+      expect(response["data"].size).to eq 1
+    end
+
+    it 'has the correct id' do
+      expect(retrieved_person["id"]).to eq person.id
+    end
+
+    it 'has the correct name' do
+      expect(retrieved_person["name"]).to eq person.name
+    end
+
+    it 'has the correct gender' do
+      expect(retrieved_person["gender"]).to eq person.gender
+    end
+
+    it 'has the correct age' do
+      expect(retrieved_person["age"]).to eq person.age
+    end
+  end
+
+  describe 'when spoofing! using a whitelist' do
+    let!(:person)          { create(:person_with_buckets) }
+    let(:people)           { Person.all }
+    let(:response)         { ModelHelper.spoof!(Person, people, whitelist: :created_at) }
+    let(:retrieved_person) { response["data"].first }
+
+    it_behaves_like 'fastapi_meta' do
+      let(:expected) { { total: 1, count: 1, offset: 0, error: false } }
+    end
+
+    it_behaves_like 'fastapi_data' do
+      let(:expected) { { attributes: %w(id name gender age created_at) } }
+    end
+  end
+
+  describe 'when spoofing! using preload' do
+    let!(:person)          { create(:person_with_buckets) }
+    let(:people)           { Person.eager_load(:buckets) }
+    let(:response)         { ModelHelper.spoof!(Person, people) }
+    let(:retrieved_person) { response["data"].first }
+
+    it_behaves_like 'fastapi_meta' do
+      let(:expected) { { total: 1, count: 1, offset: 0, error: false } }
+    end
+
+    it_behaves_like 'fastapi_data' do
+      let(:expected) { { attributes: %w(id name gender age buckets) } }
+    end
+
+    it 'has the correct count' do
+      expect(response["data"].size).to eq 1
+    end
+
+    it 'has the correct id' do
+      expect(retrieved_person["id"]).to eq person.id
+    end
+
+    it 'has the correct name' do
+      expect(retrieved_person["name"]).to eq person.name
+    end
+
+    it 'has the correct gender' do
+      expect(retrieved_person["gender"]).to eq person.gender
+    end
+
+    it 'has the correct age' do
+      expect(retrieved_person["age"]).to eq person.age
+    end
+
+    describe 'preloaded buckets' do
+      let(:buckets)             { retrieved_person["buckets"] }
+      let(:associated_bucket)   { buckets.first }
+      let(:actual_first_bucket) { Bucket.find(associated_bucket["id"]) }
+
+      it_behaves_like 'fastapi_data' do
+        let(:expected) { { data: associated_bucket, attributes: %w(id color material used) } }
+      end
+
+      it 'has five buckets' do
+        expect(buckets.size).to eq person.buckets.size
+      end
+
+      it 'the first bucket has the correct id' do
+        expect(associated_bucket["id"]).to eq actual_first_bucket.id
+      end
+
+      it 'the first bucket has the correct color' do
+        expect(associated_bucket["color"]).to eq actual_first_bucket.color
+      end
+
+      it 'the first bucket has the correct material' do
+        expect(associated_bucket["material"]).to eq actual_first_bucket.material
+      end
+
+      it 'the first bucket has the correct used value' do
+        expect(associated_bucket["used"]).to eq actual_first_bucket.used
+      end
+    end
+  end
 end
